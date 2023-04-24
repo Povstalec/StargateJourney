f1174f0e2b1723f264201ab52870b1474aaae10e
Povstalec <64008165+Povstalec@users.noreply.github.com>
2023-04-24 21:56:40 +0200

    Fixed Universe Stargate rotation rendering

--- src/main/java/net/povstalec/sgjourney/client/models/UniverseStargateModel.java
+++ src/main/java/net/povstalec/sgjourney/client/models/UniverseStargateModel.java
@@ -43,13 +43,20 @@ public class UniverseStargateModel extends AbstractStargateModel
 	public void renderStargate(UniverseStargateEntity stargate, float partialTick, PoseStack stack, MultiBufferSource source, 
 			int combinedLight, int combinedOverlay)
 	{
-		VertexConsumer ringTexture = source.getBuffer(RenderType.entitySolid(RING_TEXTURE));
+		this.renderRing(stargate, stack, source, combinedLight, combinedOverlay);
 		
-		this.ring.render(stack, ringTexture, combinedLight, combinedOverlay);
-
 		this.renderSymbolRing(stargate, stack, source, combinedLight, combinedOverlay, combinedOverlay);
 
 		this.renderChevrons(stargate, stack, source, combinedLight, combinedOverlay, combinedOverlay);
+	}
+	
+	protected void renderRing(UniverseStargateEntity stargate, PoseStack stack, MultiBufferSource source, 
+			int combinedLight, int combinedOverlay)
+	{
+		VertexConsumer ringTexture = source.getBuffer(RenderType.entitySolid(RING_TEXTURE));
+		
+		this.ring.setRotation(0.0F, 0.0F, (float) Math.toRadians(this.rotation));
+		this.ring.render(stack, ringTexture, combinedLight, combinedOverlay);
 	}
 	
 	protected void renderSymbolRing(UniverseStargateEntity stargate, PoseStack stack, MultiBufferSource source, 
@@ -57,7 +64,6 @@ public class UniverseStargateModel extends AbstractStargateModel
 	{
 		VertexConsumer symbolRingTexture = source.getBuffer(RenderType.entitySolid(SYMBOL_RING_TEXTURE));
 		
-		this.ring.setRotation(0.0F, 0.0F, (float) Math.toRadians(this.rotation));
 		for(int i = 0; i < 9; i++)
 		{
 			this.getUnderChevronLeft(i).setRotation(0.0F, 0.0F, (float) Math.toRadians(180 - (angle / 2) - (40 * i) + this.rotation));
