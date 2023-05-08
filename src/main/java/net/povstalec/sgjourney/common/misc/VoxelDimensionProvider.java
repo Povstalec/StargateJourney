4a08c5a9472afdb09ba7ced6d987a6a8b1ad9a18
TakeruDavis <flidrl@seznam.cz>
2023-05-08 20:37:03 +0200

    Cleanup of unused values

--- src/main/java/net/povstalec/sgjourney/common/misc/VoxelDimensionProvider.java
+++ src/main/java/net/povstalec/sgjourney/common/misc/VoxelDimensionProvider.java
@@ -6,52 +6,16 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class VoxelDimensionProvider {
 
-    public static final double MIN = 0.0D;
-    public static final double MAX = 16.0D;
-    public static final double MID = MAX / 2;
-    public static final double HORIZONTAL_OFFSET = 1.0D;
-    public final double HORIZONTAL_TOP;
-    public final double VERTICAL_START;
-    public final double VERTICAL_END;
+    private static final double MIN = 0.0D;
+    private static final double MAX = 16.0D;
+    private static final double MID = MAX / 2;
+    private static final double HORIZONTAL_OFFSET = 1.0D;
 
-
-
-    public final VoxelShape FULL_BLOCK;
     public final VoxelShape HORIZONTAL;
-    public final VoxelShape HORIZONTAL_BOTTOM_LEFT;
-    public final VoxelShape HORIZONTAL_BOTTOM_RIGHT;
-    public final VoxelShape HORIZONTAL_TOP_LEFT;
-    public final VoxelShape HORIZONTAL_TOP_RIGHT;
-
-    public final VoxelShape HORIZONTAL_STAIR_BOTTOM_LEFT;
-    public final VoxelShape HORIZONTAL_STAIR_BOTTOM_RIGHT;
-    public final VoxelShape HORIZONTAL_STAIR_TOP_LEFT;
-    public final VoxelShape HORIZONTAL_STAIR_TOP_RIGHT;
-
-    public final VoxelShape[] HORIZONTAL_SHAPES;
-    public final VoxelShape[] HORIZONTAL_STAIR_SHAPES;
 
     public final VoxelShape X;
-    public final VoxelShape X_TOP_LEFT;
-    public final VoxelShape X_TOP_RIGHT;
-    public final VoxelShape X_BOTTOM_LEFT;
-    public final VoxelShape X_BOTTOM_RIGHT;
-
-    public final VoxelShape X_STAIR_TOP_LEFT;
-    public final VoxelShape X_STAIR_TOP_RIGHT;
-    public final VoxelShape X_STAIR_BOTTOM_LEFT;
-    public final VoxelShape X_STAIR_BOTTOM_RIGHT;
 
     public final VoxelShape Z;
-    public final VoxelShape Z_TOP_LEFT;
-    public final VoxelShape Z_TOP_RIGHT;
-    public final VoxelShape Z_BOTTOM_LEFT;
-    public final VoxelShape Z_BOTTOM_RIGHT;
-
-    public final VoxelShape Z_STAIR_TOP_LEFT;
-    public final VoxelShape Z_STAIR_TOP_RIGHT;
-    public final VoxelShape Z_STAIR_BOTTOM_LEFT;
-    public final VoxelShape Z_STAIR_BOTTOM_RIGHT;
 
     public final VoxelShape[][] DEFAULT;
 
@@ -68,58 +32,58 @@ public class VoxelDimensionProvider {
     public VoxelDimensionProvider() {
         double width = 7.0D;
 
-        HORIZONTAL_TOP = HORIZONTAL_OFFSET + width;
-        VERTICAL_START = MID - (width / 2);
-        VERTICAL_END = MID + (width / 2);
+        double horizontalTop = HORIZONTAL_OFFSET + width;
+        double verticalStart = MID - (width / 2);
+        double verticalEnd = MID + (width / 2);
 
-        FULL_BLOCK = Block.box(MIN, MIN, MIN, MAX, MAX, MAX);
-        HORIZONTAL = Block.box(MIN, HORIZONTAL_OFFSET, MIN, MAX, HORIZONTAL_TOP, MAX);
-        HORIZONTAL_BOTTOM_LEFT = Block.box(MIN, HORIZONTAL_OFFSET, MIN, HORIZONTAL_TOP, HORIZONTAL_TOP, HORIZONTAL_TOP);
-        HORIZONTAL_BOTTOM_RIGHT = Block.box(HORIZONTAL_TOP, HORIZONTAL_OFFSET, MIN, MAX, HORIZONTAL_TOP, HORIZONTAL_TOP);
-        HORIZONTAL_TOP_LEFT = Block.box(MIN, HORIZONTAL_OFFSET, HORIZONTAL_TOP, HORIZONTAL_TOP, HORIZONTAL_TOP, MAX);
-        HORIZONTAL_TOP_RIGHT = Block.box(HORIZONTAL_TOP, HORIZONTAL_OFFSET, HORIZONTAL_TOP, MAX, HORIZONTAL_TOP, MAX);
+//        VoxelShape FULL_BLOCK = Block.box(MIN, MIN, MIN, MAX, MAX, MAX);
+        HORIZONTAL = Block.box(MIN, HORIZONTAL_OFFSET, MIN, MAX, horizontalTop, MAX);
+        VoxelShape horizontalBottomLeft = Block.box(MIN, HORIZONTAL_OFFSET, MIN, horizontalTop, horizontalTop, horizontalTop);
+        VoxelShape horizontalBottomRight = Block.box(horizontalTop, HORIZONTAL_OFFSET, MIN, MAX, horizontalTop, horizontalTop);
+        VoxelShape horizontalTopLeft = Block.box(MIN, HORIZONTAL_OFFSET, horizontalTop, horizontalTop, horizontalTop, MAX);
+        VoxelShape horizontalTopRight = Block.box(horizontalTop, HORIZONTAL_OFFSET, horizontalTop, MAX, horizontalTop, MAX);
 
-        HORIZONTAL_STAIR_BOTTOM_LEFT = Shapes.or(HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_LEFT, HORIZONTAL_TOP_RIGHT);
-        HORIZONTAL_STAIR_BOTTOM_RIGHT = Shapes.or(HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_TOP_LEFT, HORIZONTAL_TOP_RIGHT);
-        HORIZONTAL_STAIR_TOP_LEFT = Shapes.or(HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT);
-        HORIZONTAL_STAIR_TOP_RIGHT = Shapes.or(HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_LEFT);
+        VoxelShape horizontalStairBottomLeft = Shapes.or(horizontalBottomRight, horizontalTopLeft, horizontalTopRight);
+        VoxelShape horizontalStairBottomRight = Shapes.or(horizontalBottomLeft, horizontalTopLeft, horizontalTopRight);
+        VoxelShape horizontalStairTopLeft = Shapes.or(horizontalBottomLeft, horizontalBottomRight, horizontalTopRight);
+        VoxelShape horizontalStairTopRight = Shapes.or(horizontalBottomLeft, horizontalBottomRight, horizontalTopLeft);
 
-        HORIZONTAL_SHAPES = new VoxelShape[] {HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT};
-        HORIZONTAL_STAIR_SHAPES = new VoxelShape[] {HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT};
+//        VoxelShape[] HORIZONTAL_SHAPES = new VoxelShape[]{horizontalBottomRight, horizontalBottomLeft, horizontalTopRight, horizontalTopLeft};
+//        VoxelShape[] HORIZONTAL_STAIR_SHAPES = new VoxelShape[]{horizontalStairTopLeft, horizontalStairTopRight, horizontalStairBottomLeft, horizontalStairBottomRight};
 
-        X = Block.box(MIN, MIN, VERTICAL_START, MAX, MAX, VERTICAL_END);
-        X_TOP_LEFT = Block.box(MIN, MID, VERTICAL_START, MID, MAX, VERTICAL_END);
-        X_TOP_RIGHT = Block.box(MID, MID, VERTICAL_START, MAX, MAX, VERTICAL_END);
-        X_BOTTOM_LEFT = Block.box(MIN, MIN, VERTICAL_START, MID, MID, VERTICAL_END);
-        X_BOTTOM_RIGHT = Block.box(MID, MIN, VERTICAL_START, MAX, MID, VERTICAL_END);
+        X = Block.box(MIN, MIN, verticalStart, MAX, MAX, verticalEnd);
+        VoxelShape xTopLeft = Block.box(MIN, MID, verticalStart, MID, MAX, verticalEnd);
+        VoxelShape xTopRight = Block.box(MID, MID, verticalStart, MAX, MAX, verticalEnd);
+        VoxelShape xBottomLeft = Block.box(MIN, MIN, verticalStart, MID, MID, verticalEnd);
+        VoxelShape xBottomRight = Block.box(MID, MIN, verticalStart, MAX, MID, verticalEnd);
 
-        X_STAIR_TOP_LEFT =  Shapes.or(X_BOTTOM_LEFT, X_BOTTOM_RIGHT, X_TOP_RIGHT);
-        X_STAIR_TOP_RIGHT =  Shapes.or(X_BOTTOM_LEFT, X_BOTTOM_RIGHT, X_TOP_LEFT);
-        X_STAIR_BOTTOM_LEFT =  Shapes.or(X_TOP_LEFT, X_TOP_RIGHT, X_BOTTOM_RIGHT);
-        X_STAIR_BOTTOM_RIGHT =  Shapes.or(X_TOP_LEFT, X_TOP_RIGHT, X_BOTTOM_LEFT);
+        VoxelShape xStairTopLeft = Shapes.or(xBottomLeft, xBottomRight, xTopRight);
+        VoxelShape xStairTopRight = Shapes.or(xBottomLeft, xBottomRight, xTopLeft);
+        VoxelShape xStairBottomLeft = Shapes.or(xTopLeft, xTopRight, xBottomRight);
+        VoxelShape xStairBottomRight = Shapes.or(xTopLeft, xTopRight, xBottomLeft);
 
-        Z = Block.box(VERTICAL_START, MIN, MIN, VERTICAL_END, MAX, MAX);
-        Z_TOP_LEFT = Block.box(VERTICAL_START, MID, MIN, VERTICAL_END, MAX, MID);
-        Z_TOP_RIGHT = Block.box(VERTICAL_START, MID, MID, VERTICAL_END, MAX, MAX);
-        Z_BOTTOM_LEFT = Block.box(VERTICAL_START, MIN, MIN, VERTICAL_END, MID, MID);
-        Z_BOTTOM_RIGHT = Block.box(VERTICAL_START, MIN, MID, VERTICAL_END, MID, MAX);
+        Z = Block.box(verticalStart, MIN, MIN, verticalEnd, MAX, MAX);
+        VoxelShape zTopLeft = Block.box(verticalStart, MID, MIN, verticalEnd, MAX, MID);
+        VoxelShape zTopRight = Block.box(verticalStart, MID, MID, verticalEnd, MAX, MAX);
+        VoxelShape zBottomLeft = Block.box(verticalStart, MIN, MIN, verticalEnd, MID, MID);
+        VoxelShape zBottomRight = Block.box(verticalStart, MIN, MID, verticalEnd, MID, MAX);
 
-        Z_STAIR_TOP_LEFT =  Shapes.or(Z_BOTTOM_LEFT, Z_BOTTOM_RIGHT, Z_TOP_RIGHT);
-        Z_STAIR_TOP_RIGHT =  Shapes.or(Z_BOTTOM_LEFT, Z_BOTTOM_RIGHT, Z_TOP_LEFT);
-        Z_STAIR_BOTTOM_LEFT =  Shapes.or(Z_TOP_LEFT, Z_TOP_RIGHT, Z_BOTTOM_RIGHT);
-        Z_STAIR_BOTTOM_RIGHT =  Shapes.or(Z_TOP_LEFT, Z_TOP_RIGHT, Z_BOTTOM_LEFT);
+        VoxelShape zStairTopLeft = Shapes.or(zBottomLeft, zBottomRight, zTopRight);
+        VoxelShape zStairTopRight = Shapes.or(zBottomLeft, zBottomRight, zTopLeft);
+        VoxelShape zStairBottomLeft = Shapes.or(zTopLeft, zTopRight, zBottomRight);
+        VoxelShape zStairBottomRight = Shapes.or(zTopLeft, zTopRight, zBottomLeft);
 
         DEFAULT = new VoxelShape[][] {{HORIZONTAL}, {X, Z}, {HORIZONTAL}};
 
-        TOP_LEFT = new VoxelShape[][] {{HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT}, {X_TOP_LEFT, Z_TOP_LEFT, X_TOP_RIGHT, Z_TOP_RIGHT}, {HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT}};
-        BOTTOM_LEFT = new VoxelShape[][] {{HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT}, {X_BOTTOM_LEFT, Z_BOTTOM_LEFT, X_BOTTOM_RIGHT, Z_BOTTOM_RIGHT}, {HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT}};
-        BOTTOM_RIGHT = new VoxelShape[][] {{HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT}, {X_BOTTOM_RIGHT, Z_BOTTOM_RIGHT, X_BOTTOM_LEFT, Z_BOTTOM_LEFT}, {HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT}};
-        TOP_RIGHT = new VoxelShape[][] {{HORIZONTAL_BOTTOM_RIGHT, HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT}, {X_TOP_RIGHT, Z_TOP_RIGHT, X_TOP_LEFT, Z_TOP_LEFT}, {HORIZONTAL_TOP_RIGHT, HORIZONTAL_TOP_LEFT, HORIZONTAL_BOTTOM_LEFT, HORIZONTAL_BOTTOM_RIGHT}};
+        TOP_LEFT = new VoxelShape[][] {{horizontalBottomLeft, horizontalBottomRight, horizontalTopRight, horizontalTopLeft}, {xTopLeft, zTopLeft, xTopRight, zTopRight}, {horizontalTopLeft, horizontalBottomLeft, horizontalBottomRight, horizontalTopRight}};
+        BOTTOM_LEFT = new VoxelShape[][] {{horizontalTopLeft, horizontalBottomLeft, horizontalBottomRight, horizontalTopRight}, {xBottomLeft, zBottomLeft, xBottomRight, zBottomRight}, {horizontalBottomLeft, horizontalBottomRight, horizontalTopRight, horizontalTopLeft}};
+        BOTTOM_RIGHT = new VoxelShape[][] {{horizontalTopRight, horizontalTopLeft, horizontalBottomLeft, horizontalBottomRight}, {xBottomRight, zBottomRight, xBottomLeft, zBottomLeft}, {horizontalBottomRight, horizontalTopRight, horizontalTopLeft, horizontalBottomLeft}};
+        TOP_RIGHT = new VoxelShape[][] {{horizontalBottomRight, horizontalTopRight, horizontalTopLeft, horizontalBottomLeft}, {xTopRight, zTopRight, xTopLeft, zTopLeft}, {horizontalTopRight, horizontalTopLeft, horizontalBottomLeft, horizontalBottomRight}};
 
-        STAIR_TOP_LEFT = new VoxelShape[][] {{HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT}, {X_STAIR_TOP_LEFT, Z_STAIR_TOP_LEFT, X_STAIR_TOP_RIGHT, Z_STAIR_TOP_RIGHT}, {HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT}};
-        STAIR_BOTTOM_LEFT = new VoxelShape[][] {{HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT}, {X_STAIR_BOTTOM_LEFT, Z_STAIR_BOTTOM_LEFT, X_STAIR_BOTTOM_RIGHT, Z_STAIR_BOTTOM_RIGHT}, {HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT}};
-        STAIR_BOTTOM_RIGHT = new VoxelShape[][] {{HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT}, {X_STAIR_BOTTOM_RIGHT, Z_STAIR_BOTTOM_RIGHT, X_STAIR_BOTTOM_LEFT, Z_STAIR_BOTTOM_LEFT}, {HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT}};
-        STAIR_TOP_RIGHT = new VoxelShape[][] {{HORIZONTAL_STAIR_BOTTOM_RIGHT, HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT}, {X_STAIR_TOP_RIGHT, Z_STAIR_TOP_RIGHT, X_STAIR_TOP_LEFT, Z_STAIR_TOP_LEFT}, {HORIZONTAL_STAIR_TOP_RIGHT, HORIZONTAL_STAIR_TOP_LEFT, HORIZONTAL_STAIR_BOTTOM_LEFT, HORIZONTAL_STAIR_BOTTOM_RIGHT}};
+        STAIR_TOP_LEFT = new VoxelShape[][] {{horizontalStairBottomLeft, horizontalStairBottomRight, horizontalStairTopRight, horizontalStairTopLeft}, {xStairTopLeft, zStairTopLeft, xStairTopRight, zStairTopRight}, {horizontalStairTopLeft, horizontalStairBottomLeft, horizontalStairBottomRight, horizontalStairTopRight}};
+        STAIR_BOTTOM_LEFT = new VoxelShape[][] {{horizontalStairTopLeft, horizontalStairBottomLeft, horizontalStairBottomRight, horizontalStairTopRight}, {xStairBottomLeft, zStairBottomLeft, xStairBottomRight, zStairBottomRight}, {horizontalStairBottomLeft, horizontalStairBottomRight, horizontalStairTopRight, horizontalStairTopLeft}};
+        STAIR_BOTTOM_RIGHT = new VoxelShape[][] {{horizontalStairTopRight, horizontalStairTopLeft, horizontalStairBottomLeft, horizontalStairBottomRight}, {xStairBottomRight, zStairBottomRight, xStairBottomLeft, zStairBottomLeft}, {horizontalStairBottomRight, horizontalStairTopRight, horizontalStairTopLeft, horizontalStairBottomLeft}};
+        STAIR_TOP_RIGHT = new VoxelShape[][] {{horizontalStairBottomRight, horizontalStairTopRight, horizontalStairTopLeft, horizontalStairBottomLeft}, {xStairTopRight, zStairTopRight, xStairTopLeft, zStairTopLeft}, {horizontalStairTopRight, horizontalStairTopLeft, horizontalStairBottomLeft, horizontalStairBottomRight}};
     }
 
 }
