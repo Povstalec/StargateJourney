61ef4bef22897c9f7b8ba07573edddad8f8ae2d7
Povstalec <64008165+Povstalec@users.noreply.github.com>
2023-10-01 11:13:51 +0200

    Silly change

--- src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
+++ src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
@@ -5,7 +5,6 @@ import org.joml.Matrix4f;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
 
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
