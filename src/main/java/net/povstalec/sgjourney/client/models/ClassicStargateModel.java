1a7dc5bd4b549bb3762021b46ffed09483c4f0c1
Povstalec <64008165+Povstalec@users.noreply.github.com>
2023-10-01 11:13:01 +0200

    Annoying fix

--- src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
+++ src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
@@ -1,11 +1,9 @@
 package net.povstalec.sgjourney.client.models;
 
-import org.joml.Matrix3f;
-import org.joml.Matrix4f;
-
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Axis;
+import com.mojang.math.Matrix3f;
+import com.mojang.math.Matrix4f;
 
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
