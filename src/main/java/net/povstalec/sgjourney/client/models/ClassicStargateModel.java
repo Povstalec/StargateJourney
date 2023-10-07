8b7851903a839b9863ae7373043c03a33d79ab96
Povstalec <64008165+Povstalec@users.noreply.github.com>
2023-10-07 17:29:57 +0200

    0.6.8

--- src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
+++ src/main/java/net/povstalec/sgjourney/client/models/ClassicStargateModel.java
@@ -5,6 +5,7 @@ import org.joml.Matrix4f;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
