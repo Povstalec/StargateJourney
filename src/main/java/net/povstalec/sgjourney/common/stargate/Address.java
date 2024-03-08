11c0bcd45165160e7cf4560a1bcc558fdcfb6e6f
Povstalec <woldericzjunior@gmail.com>
2024-03-08 14:09:12 +0100

    Added copy() to Address

--- src/main/java/net/povstalec/sgjourney/common/stargate/Address.java
+++ src/main/java/net/povstalec/sgjourney/common/stargate/Address.java
@@ -244,6 +244,13 @@ public final class Address
 		return this;
 	}
 	
+	public Address copy()
+	{
+		Address copyAddress = new Address(this.isBuffer).fromArray(addressArray);
+		copyAddress.dimension = this.dimension;
+		return copyAddress;
+	}
+	
 	public boolean containsSymbol(int symbol)
 	{
 		for(int i = 0; i < getLength(); i++)
