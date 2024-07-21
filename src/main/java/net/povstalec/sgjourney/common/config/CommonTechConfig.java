6e6b4ee7df33fd9f88eefc077008a0e5f109b243
Povstalec <woldericzjunior@gmail.com>
2024-07-22 00:02:52 +0200

    Added Crystal configs for Energy related values

--- src/main/java/net/povstalec/sgjourney/common/config/CommonTechConfig.java
+++ src/main/java/net/povstalec/sgjourney/common/config/CommonTechConfig.java
@@ -6,10 +6,46 @@ public class CommonTechConfig
 {
 	public static ForgeConfigSpec.BooleanValue disable_kara_kesh_requirements;
 	
+	public static ForgeConfigSpec.LongValue energy_crystal_capacity;
+	public static ForgeConfigSpec.LongValue advanced_energy_crystal_capacity;
+	public static ForgeConfigSpec.LongValue energy_crystal_max_transfer;
+	public static ForgeConfigSpec.LongValue advanced_energy_crystal_max_transfer;
+	
+	public static ForgeConfigSpec.LongValue transfer_crystal_max_transfer;
+	public static ForgeConfigSpec.LongValue advanced_transfer_crystal_max_transfer;
+	
 	public static void init(ForgeConfigSpec.Builder server)
 	{
 		disable_kara_kesh_requirements = server
 				.comment("If true Kara Kesh won't require its user to have Naquadah in their bloodstream")
 				.define("server.disable_kara_kesh_requirements", true);
+		
+		
+		
+		energy_crystal_capacity = server
+				.comment("The amount of energy an Energy Crystal can hold")
+				.defineInRange("server.energy_crystal_capacity", 50000L, 1L, Long.MAX_VALUE);
+		
+		advanced_energy_crystal_capacity = server
+				.comment("The amount of energy an Advanced Energy Crystal can hold")
+				.defineInRange("server.advanced_energy_crystal_capacity", 100000L, 1L, Long.MAX_VALUE);
+		
+		energy_crystal_max_transfer = server
+				.comment("The amount of energy that can be transfered into and out of an Energy Crystal per tick")
+				.defineInRange("server.energy_crystal_capacity", 1500L, 1L, Long.MAX_VALUE);
+		
+		advanced_energy_crystal_max_transfer = server
+				.comment("The amount of energy that can be transfered into and out of an Advanced Energy Crystal per tick")
+				.defineInRange("server.advanced_energy_crystal_capacity", 3000L, 1L, Long.MAX_VALUE);
+		
+		
+		
+		transfer_crystal_max_transfer = server
+				.comment("The amount of energy a Transfer Crystal can transfer per tick")
+				.defineInRange("server.transfer_crystal_max_transfer", 2500L, 1L, Long.MAX_VALUE);
+		
+		advanced_transfer_crystal_max_transfer = server
+				.comment("The amount of energy an Advanced Transfer Crystal can transfer per tick")
+				.defineInRange("server.advanced_transfer_crystal_max_transfer", 5000L, 1L, Long.MAX_VALUE);
 	}
 }
