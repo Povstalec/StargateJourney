bedee27d6e062f065ee32b1f52e47b5264198fda
Povstalec <woldericzjunior@gmail.com>
2024-09-03 15:50:23 +0200

    Full Iris textures

--- src/main/java/net/povstalec/sgjourney/client/models/IrisModel.java
+++ src/main/java/net/povstalec/sgjourney/client/models/IrisModel.java
@@ -21,22 +21,26 @@ public class IrisModel
 	private static final int TOTAL_SIDES = 20;
 	private static final float DEGREES = (float) 360 / TOTAL_SIDES;
 	
+	public static final float IRIS_TEXTURE_WIDTH = 256;
+	public static final float IRIS_TEXTURE_HEIGHT = 256;
+	
 	public static final float IRIS_BLADE_WIDTH = 0.792F;
 	public static final float IRIS_BLADE_WIDTH_HALF = IRIS_BLADE_WIDTH / 2;
 	
 	public static final float IRIS_BLADE_LENGTH = 2.5F;
+	public static final float IRIS_BLADE_HEIGHT = IRIS_BLADE_LENGTH * 16;
 	
 	public static final float IRIS_OPEN_DEGREES = 84.0F;
 	public static final float IRIS_ROTATE_DEGREES = 1.0F;
 	
-	public static final float TEXTURE_CENTER_U = 13F / 2 / 128;
+	public static final float TEXTURE_CENTER_U = 13F / 2 / IRIS_TEXTURE_WIDTH;
 	public static final float TEXTURE_TOP_V = 0;
 	
-	public static final float TEXTURE_LEFT_U = TEXTURE_CENTER_U - IRIS_BLADE_WIDTH * 16 / 2 / 128;
-	public static final float TEXTURE_LEFT_V = IRIS_BLADE_LENGTH * 16 / 128;
+	public static final float TEXTURE_LEFT_U = TEXTURE_CENTER_U - IRIS_BLADE_WIDTH * 16 / 2 / IRIS_TEXTURE_WIDTH;
+	public static final float TEXTURE_LEFT_V = IRIS_BLADE_HEIGHT / IRIS_TEXTURE_HEIGHT;
 	
-	public static final float TEXTURE_RIGHT_U = TEXTURE_CENTER_U + IRIS_BLADE_WIDTH * 16 / 2 / 128;
-	public static final float TEXTURE_RIGHT_V = IRIS_BLADE_LENGTH * 16 / 128;
+	public static final float TEXTURE_RIGHT_U = TEXTURE_CENTER_U + IRIS_BLADE_WIDTH * 16 / 2 / IRIS_TEXTURE_WIDTH;
+	public static final float TEXTURE_RIGHT_V = IRIS_BLADE_HEIGHT / IRIS_TEXTURE_HEIGHT;
 	
 	private boolean renderWhenOpen;
 	private float maxOpenDegrees;
@@ -71,6 +75,9 @@ public class IrisModel
 			Matrix4f matrix4 = stack.last().pose();
 			Matrix3f matrix3 = stack.last().normal();
 			
+			float uAddition = 13 * (j % 10) / IRIS_TEXTURE_WIDTH;
+			float vAddition = 40 * (j / 10) / IRIS_TEXTURE_WIDTH;
+			
 			// Front
 			SGJourneyModel.createTriangle(consumer, matrix4, matrix3, combinedLight,
 					0, 0, 1,
@@ -78,17 +85,17 @@ public class IrisModel
 					-IRIS_BLADE_WIDTH,
 					0,
 					OFFSET,
-					TEXTURE_RIGHT_U, TEXTURE_RIGHT_V,
+					TEXTURE_RIGHT_U + uAddition, TEXTURE_RIGHT_V + vAddition,
 					
 					-IRIS_BLADE_WIDTH_HALF,
 					-IRIS_BLADE_LENGTH,
 					OFFSET,
-					TEXTURE_CENTER_U, TEXTURE_TOP_V,
+					TEXTURE_CENTER_U + uAddition, TEXTURE_TOP_V + vAddition,
 					
 					0,
 					0,
 					OFFSET,
-					TEXTURE_LEFT_U, TEXTURE_LEFT_V);
+					TEXTURE_LEFT_U + uAddition, TEXTURE_LEFT_V + vAddition);
 			
 			// Back
 			SGJourneyModel.createTriangle(consumer, matrix4, matrix3, combinedLight, 
@@ -96,17 +103,17 @@ public class IrisModel
 					-IRIS_BLADE_WIDTH_HALF,
 					-IRIS_BLADE_LENGTH,
 					OFFSET,
-					TEXTURE_CENTER_U, TEXTURE_TOP_V,
+					TEXTURE_CENTER_U + uAddition, TEXTURE_TOP_V + vAddition,
 					
 					-IRIS_BLADE_WIDTH,
 					0,
 					OFFSET,
-					TEXTURE_RIGHT_U, TEXTURE_RIGHT_V,
+					TEXTURE_RIGHT_U + uAddition, TEXTURE_RIGHT_V + vAddition,
 					
 					0,
 					0,
 					OFFSET,
-					TEXTURE_LEFT_U, TEXTURE_LEFT_V);
+					TEXTURE_LEFT_U + uAddition, TEXTURE_LEFT_V + vAddition);
 			
 			stack.popPose();
 			
