772e5188e703eed0c1275add125ab740ae58bf60
Povstalec <woldericzjunior@gmail.com>
2024-09-07 12:48:28 +0200

    Transceiver item texture

--- src/main/java/net/povstalec/sgjourney/common/blocks/tech/AdvancedCrystalInterfaceBlock.java
+++ src/main/java/net/povstalec/sgjourney/common/blocks/tech/AdvancedCrystalInterfaceBlock.java
@@ -1,8 +1,15 @@
 package net.povstalec.sgjourney.common.blocks.tech;
 
+import java.util.List;
+
 import javax.annotation.Nullable;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -43,5 +50,11 @@ public class AdvancedCrystalInterfaceBlock extends AbstractInterfaceBlock
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type)
 	{
 		return createTickerHelper(type, BlockEntityInit.ADVANCED_CRYSTAL_INTERFACE.get(), CrystalInterfaceEntity::tick);
+    }
+	
+	@Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter getter, List<Component> tooltipComponents, TooltipFlag isAdvanced)
+    {
+		tooltipComponents.add(Component.translatable("block.sgjourney.advanced_crystal_interface.description").withStyle(ChatFormatting.GRAY));
     }
 }
