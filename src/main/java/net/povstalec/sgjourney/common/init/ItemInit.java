0e60fb4e0f6618fb1c14bbaa8392e7a10a7fff0e
Povstalec <woldericzjunior@gmail.com>
2025-03-23 13:12:25 +0100

    Version specific changes

--- src/main/java/net/povstalec/sgjourney/common/init/ItemInit.java
+++ src/main/java/net/povstalec/sgjourney/common/init/ItemInit.java
@@ -155,13 +155,13 @@ public class ItemInit
 			() -> new ArmorItem(ArmorMaterialInit.NAQUADAH, ArmorItem.Type.BOOTS, new Item.Properties().fireResistant()));
 	
 	public static final RegistryObject<ArmorItem> SYSTEM_LORD_HELMET = ITEMS.register("system_lord_helmet",
-			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, EquipmentSlot.HEAD, new Item.Properties().fireResistant()));
+			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, ArmorItem.Type.HELMET, new Item.Properties().fireResistant()));
 	public static final RegistryObject<ArmorItem> SYSTEM_LORD_CHESTPLATE = ITEMS.register("system_lord_chestplate",
-			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, EquipmentSlot.CHEST, new Item.Properties().fireResistant()));
+			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, ArmorItem.Type.CHESTPLATE, new Item.Properties().fireResistant()));
 	public static final RegistryObject<ArmorItem> SYSTEM_LORD_LEGGINGS = ITEMS.register("system_lord_leggings",
-			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, EquipmentSlot.LEGS, new Item.Properties().fireResistant()));
+			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, ArmorItem.Type.LEGGINGS, new Item.Properties().fireResistant()));
 	public static final RegistryObject<ArmorItem> SYSTEM_LORD_BOOTS = ITEMS.register("system_lord_boots",
-			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, EquipmentSlot.FEET, new Item.Properties().fireResistant()));
+			() -> new ArmorItem(ArmorMaterialInit.SYSTEM_LORD, ArmorItem.Type.BOOTS, new Item.Properties().fireResistant()));
 	
 	public static final RegistryObject<ArmorItem> JAFFA_HELMET = ITEMS.register("jaffa_helmet", 
 			() -> new ArmorItem(ArmorMaterialInit.JAFFA, ArmorItem.Type.HELMET, new Item.Properties().fireResistant()));
