5a10f0b27707a252febec476b5b1b0208060d6c0
Povstalec <woldericzjunior@gmail.com>
2025-03-23 11:40:36 +0100

    Version specific changes

--- src/main/java/net/povstalec/sgjourney/common/entities/Human.java
+++ src/main/java/net/povstalec/sgjourney/common/entities/Human.java
@@ -35,7 +35,7 @@ public class Human extends AgeableMob
 	protected void registerGoals()
 	{
 		this.goalSelector.addGoal(1, new FloatGoal(this));
-		this.goalSelector.addGoal(1, new ClimbOnTopOfPowderSnowGoal(this, this.level));
+		this.goalSelector.addGoal(1, new ClimbOnTopOfPowderSnowGoal(this, this.level()));
 		this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, false));
 		this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)).setAlertOthers());
 		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
