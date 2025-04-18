84c73421ab436187ef7b3ccf51b08f7a184cc791
Povstalec <woldericzjunior@gmail.com>
2025-04-19 01:28:32 +0200

    Common Stargate Search

--- src/main/java/net/povstalec/sgjourney/common/config/CommonGenerationConfig.java
+++ src/main/java/net/povstalec/sgjourney/common/config/CommonGenerationConfig.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.ForgeConfigSpec;
 public class CommonGenerationConfig
 {
 	public static SGJourneyConfigValue.BooleanValue common_stargate_generation;
+	public static SGJourneyConfigValue.BooleanValue common_stargate_search;
 
 	public static ForgeConfigSpec.IntValue stargate_generation_center_x_chunk_offset;
 	public static ForgeConfigSpec.IntValue stargate_generation_center_z_chunk_offset;
@@ -18,6 +19,10 @@ public class CommonGenerationConfig
 		common_stargate_generation = new SGJourneyConfigValue.BooleanValue(server, "server.common_stargate_generation",
 				false,
 				"Common Stargates will generate");
+		
+		common_stargate_search = new SGJourneyConfigValue.BooleanValue(server, "server.common_stargate_search",
+				false,
+				"When false, Stargate Network won't include Common Stargates in the search for ungenerated Stargate Structures");
 		
 		
 		
