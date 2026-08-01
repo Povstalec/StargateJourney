b3db6e26e7501d33f0dd4e76454091684dd0fd7d
Povstalec <woldericzjunior@gmail.com>
2026-08-01 19:35:13 +0200

    Transporer blacklist tag

--- src/main/java/net/povstalec/sgjourney/common/sgjourney/Transporting.java
+++ src/main/java/net/povstalec/sgjourney/common/sgjourney/Transporting.java
@@ -13,6 +13,7 @@ import net.minecraft.world.phys.Vec3;
 import net.povstalec.sgjourney.common.advancements.TransporterTravelCriterion;
 import net.povstalec.sgjourney.common.events.custom.SGJourneyEvents;
 import net.povstalec.sgjourney.common.init.StatisticsInit;
+import net.povstalec.sgjourney.common.init.TagInit;
 import net.povstalec.sgjourney.common.misc.CoordinateHelper;
 import net.povstalec.sgjourney.common.sgjourney.transporter.Transporter;
 
@@ -27,13 +28,21 @@ public class Transporting
 	//***************************************Transport out****************************************
 	//============================================================================================
 	
+	public static boolean shouldTransport(Entity traveler, Vec3 relativePosition, double innerRadius)
+	{
+		if(traveler.isPassenger() || traveler.getType().is(TagInit.Entities.TRANSPORTER_CANNOT_TELEPORT))
+			return false;
+		
+		return relativePosition.x() * relativePosition.z() <= innerRadius * innerRadius;
+	}
+	
 	public static boolean transportTraveler(MinecraftServer server, TransporterConnection connection, Transporter initialTransporter, Transporter receivingTransporter, Entity traveler)
 	{
 		Vec3 relativePosition = initialTransporter.toTransporterCoords(traveler.position().subtract(initialTransporter.transportPos()), true);
 		Vec3 relativeMomentum = initialTransporter.toTransporterCoords(traveler.getDeltaMovement(), false);
 		Vec3 relativeLookAngle = initialTransporter.toTransporterCoords(traveler.getLookAngle(), false);
 		
-		if(relativePosition.lengthSqr() <= initialTransporter.getInnerRadius() * initialTransporter.getInnerRadius())
+		if(shouldTransport(traveler, relativePosition, initialTransporter.getInnerRadius()))
 		{
 			if(!SGJourneyEvents.onTransporterTransport(server, initialTransporter, receivingTransporter, traveler) && receivingTransporter.receiveTraveler(connection, initialTransporter, traveler, relativePosition, relativeMomentum, relativeLookAngle))
 			{
